005ce7eba0eb1f6704dab25f785190956b2d9742
otvertka <mazilla_D@tut.by>
2017-09-03 17:58:38 +0300

    1 клик - появляются 2 рандомных числа(100) 2 клик - появляется ответ.

--- app/src/main/java/com/example/multiplication/MainActivity.java
+++ app/src/main/java/com/example/multiplication/MainActivity.java
@@ -2,12 +2,54 @@ package com.example.multiplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Random random = new Random();
+
+    TextView tvNumber1;
+    TextView tvNumber2;
+    TextView tvResult;
+
+    Button btn;
+
+    int a, b, c;
+
+    boolean proverka = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvNumber1 = (TextView)findViewById(R.id.tvNumber1);
+        tvNumber2 = (TextView)findViewById(R.id.tvNumber2);
+        tvResult = (TextView)findViewById(R.id.tvResult);
+
+        btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(this);
+    }
+
+
+
+    @Override
+    public void onClick(View view) {
+        if (!proverka){
+            a = random.nextInt(100) + 1;
+            tvNumber1.setText(String.valueOf(a));
+            b = random.nextInt(100) + 1;
+            tvNumber2.setText(String.valueOf(b));
+            c = a * b;
+            tvResult.setText("Ответ...");
+            proverka = true;
+        } else {
+            tvResult.setText(String.valueOf(c));
+            proverka = false;
+        }
     }
 }
