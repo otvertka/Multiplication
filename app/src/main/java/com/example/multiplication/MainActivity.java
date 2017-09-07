f111033984e14452a816c248e153c582e1a85f5d
otvertka <mazilla_D@tut.by>
2017-09-07 21:05:29 +0300

    Добавил обработчик кнопок, при правльном наборе ответа пишет УРА

--- app/src/main/java/com/example/multiplication/MainActivity.java
+++ app/src/main/java/com/example/multiplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -16,7 +17,7 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     Random random = new Random();
 
@@ -26,8 +27,12 @@ public class MainActivity extends AppCompatActivity{
     TextView tvNumber2;
     @BindView(R.id.tvResult)
     TextView tvResult;
+    @BindView(R.id.tv)
+    TextView tv;
+    @BindView(R.id.editText)
+    EditText editText;
 
-    @BindView(R.id.btn0)
+    /*@BindView(R.id.btn0)
     Button btn0;
     @BindView(R.id.btn1)
     Button btn1;
@@ -46,7 +51,7 @@ public class MainActivity extends AppCompatActivity{
     @BindView(R.id.btn8)
     Button btn8;
     @BindView(R.id.btn9)
-    Button btn9;
+    Button btn9;*/
 
     int a, b, c;
 
@@ -58,6 +63,47 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+    }
+
+    @OnClick({R.id.btn0, R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4, R.id.btn5, R.id.btn6, R.id.btn7, R.id.btn8, R.id.btn9})
+    void onSaveClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.btn0:
+                tv.setText("" + tv.getText() + 0);
+                break;
+            case R.id.btn1:
+                tv.setText("" + tv.getText() + 1);
+                break;
+            case R.id.btn2:
+                tv.setText("" + tv.getText() + 2);
+                break;
+            case R.id.btn3:
+                tv.setText("" + tv.getText() + 3);
+                break;
+            case R.id.btn4:
+                tv.setText("" + tv.getText() + 4);
+                break;
+            case R.id.btn5:
+                tv.setText("" + tv.getText() + 5);
+                break;
+            case R.id.btn6:
+                tv.setText("" + tv.getText() + 6);
+                break;
+            case R.id.btn7:
+                tv.setText("" + tv.getText() + 7);
+                break;
+            case R.id.btn8:
+                tv.setText("" + tv.getText() + 8);
+                break;
+            case R.id.btn9:
+                //Toast.makeText(getApplicationContext(), "Press :" + 9, Toast.LENGTH_SHORT).show();
+                tv.setText("" + tv.getText() + 9);
+                break;
+        }
+        if(tv.getText().equals(String.valueOf(c))){
+            Toast.makeText(getApplicationContext(), "УРАААА" , Toast.LENGTH_SHORT).show();
+        }
     }
 
     @OnClick(R.id.button)
@@ -72,6 +118,7 @@ public class MainActivity extends AppCompatActivity{
             proverka = true;
         } else {
             tvResult.setText(String.valueOf(c));
+            editText.setText(String.valueOf(c));
             proverka = false;
         }
     }
